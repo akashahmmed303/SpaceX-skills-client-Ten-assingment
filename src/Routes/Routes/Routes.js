fd12882cf2c91a2b84be0be579155cb36be72462
Akash Ahmmed <96370993+akashahmmed303@users.noreply.github.com>
2022-10-28 23:24:58 +0600

    Update Routes.js

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -27,7 +27,7 @@ export const routes = createBrowserRouter([
           </PrivateRoute>
         ),
         loader: ({ params }) =>
-          fetch(`https://space-x-skills-server.vercel.app/course/${params.id}`),
+          fetch(`https://space-x-skills-server.vercel.app/${params.id}`),
       },
       {
         path: '/faq/:id',
